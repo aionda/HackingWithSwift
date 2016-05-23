094ed46c6a00b5bd6b05f2c556f66b29f9c30958
Ai Onda <1onda.ai@gmail.com>
2016-05-23 15:11:33 -0400

    added flag buttons to interface builder

---
+++ Project2/Project2/Project2/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Ai Onda on 5/23/16.
+//  Copyright © 2016 Ai Onda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
